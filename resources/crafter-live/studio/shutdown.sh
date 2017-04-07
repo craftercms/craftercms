ba37bdfa2208dac6d1f141866c0d52951c60ec78
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-04-07 15:04:29 -0600

    Configurable Ports for Authoring Env

--- resources/crafter-live/studio/shutdown.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-./crafter.sh --stop
--- resources/crafter/crafter.sh
+++ resources/crafter/crafter.sh
@@ -19,16 +19,16 @@ function debug() {
     cd $CD_HOME
      ./deployer.sh --debug;
      cd $C_HOME
-     echo "Starting Solr server on port 8994"
-     ./solr/bin/solr start -p 8984 -Dcrafter.solr.index=$C_HOME/data/indexes -a "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044" &
+     echo "Starting Solr server on port @SOLR_PORT@"
+     ./solr/bin/solr start -p @SOLR_PORT@ -Dcrafter.solr.index=$C_HOME/data/indexes -a "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=@SOLR_PORT_D@" &
      ./apache-tomcat/bin/catalina.sh jpda start;
 }
 function start() {
     cd $CD_HOME
      ./deployer.sh --start;
      cd $C_HOME
-     echo "Starting Solr server on port 8994"
-     ./solr/bin/solr start -p 8984 -Dcrafter.solr.index=$C_HOME/data/indexes &
+     echo "Starting Solr server on port @SOLR_PORT@"
+     ./solr/bin/solr start -p @SOLR_PORT@ -Dcrafter.solr.index=$C_HOME/data/indexes &
      ./apache-tomcat/bin/startup.sh
 }
 
