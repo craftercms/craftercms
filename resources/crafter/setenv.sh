2feb736d358c449b6e1bf881ac156d50e94fb7a8
Jose Ross <jdross17@gmail.com>
2017-05-19 14:22:47 -0600

    Updated tomcat logging configuration.

--- resources/crafter/setenv.sh
+++ resources/crafter/setenv.sh
@@ -5,7 +5,7 @@ export CATALINA_HOME="$CRAFTER_HOME/apache-tomcat"
 export CATALINA_PID=$CATALINA_HOME/tomcat.pid
 export CATALINA_LOGS_DIR=$CRAFTER_ROOT/logs/tomcat
 export CATALINA_OUT=$CATALINA_LOGS_DIR/catalina.out
-export CATALINA_JAVA_OPTS="-Dcatalina.logs=$CATALINA_LOGS_DIR -server -Xss1024K -Xms1G -Xmx4G"
+export CATALINA_OPTS="-Dcatalina.logs=$CATALINA_LOGS_DIR -server -Xss1024K -Xms1G -Xmx4G"
 # Solr variables
 export SOLR_PORT=@SOLR_PORT@
 export SOLR_INDEXES_DIR=$CRAFTER_ROOT/data/indexes
