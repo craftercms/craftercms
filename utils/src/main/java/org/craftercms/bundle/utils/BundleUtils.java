7c945d007cb7d3cafc2c0842440e86cb7c8ae483
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-06-01 09:59:44 -0600

    Solves #890

--- utils/src/main/java/org/craftercms/bundle/utils/BundleUtils.java
+++ utils/src/main/java/org/craftercms/bundle/utils/BundleUtils.java
@@ -1,46 +1,64 @@
 package org.craftercms.bundle.utils;
 
-import org.craftercms.bundle.utils.actions.Download;
-import org.craftercms.bundle.utils.actions.Unzip;
-
 import java.util.Arrays;
 import java.util.HashMap;
+
+import org.craftercms.bundle.utils.actions.Download;
+import org.craftercms.bundle.utils.actions.Unzip;
 
 /**
  * Created by cortiz on 4/27/17.
  */
 public class BundleUtils {
-    static HashMap<String, Action> action=new HashMap<>();
-    static{
-        action.put("download",new Download());
-        action.put("unzip",new Unzip());
+
+    /**
+     * Registered Actions.
+     */
+    private static HashMap<String, Action> action = new HashMap<>();
+
+    static {
+        action.put("download", new Download());
+        action.put("unzip", new Unzip());
     }
-    public static void main(String[] args) {
-        if(args.length<=0){
+
+    /**
+     * All start's here.
+     *
+     * @param args arguments.
+     */
+    public static void main(final String[] args) {
+        if (args.length <= 0) {
             helpAndExit();
         }
         String actionToDo = args[0].toLowerCase();
-        if(actionToDo.equals("help") && args.length>=1) {
+        if ("help".equals(actionToDo) && args.length >= 1) {
             String actionToHelp = args[1].toLowerCase();
             if (action.containsKey(actionToHelp)) {
                 action.get(actionToHelp).help();
             } else {
                 helpAndExit();
             }
-        }else if(action.containsKey(actionToDo)){
-            action.get(actionToDo).execute(Arrays.copyOfRange(args,1,args.length));
-        }else{
-            helpAndExit();
+        } else if (action.containsKey(actionToDo)) {
+            action.get(actionToDo).execute(Arrays.copyOfRange(args, 1, args.length));
+        } else {
+            helpAndExit(); //NOPMD
         }
     }
 
-    private static void helpAndExit(){
+    /**
+     * Print help message and exit.
+     */
+    private static void helpAndExit() {
         help();
-        System.exit(-1);
+        System.exit(-1); //NOPMD
     }
 
+    /**
+     * Print the error message.
+     */
     private static void help() {
-        System.out.printf("Crafter CMS bundle Utils %s-%s \n",Version.BUILD,Version.BUILD_ID.substring(0,6));
+        System.out.printf("Crafter CMS bundle Utils %s-%s \n", org.craftercms.bundle.utils.Version.BUILD, org
+            .craftercms.bundle.utils.Version.BUILD_ID.substring(0, 6));
         System.out.println("Usage: java -jar craftercms-utils.jar {Action} {Action Params}");
         System.out.println("Current actions: download help unzip");
         System.out.println("Use help {Action} for more info about the action");
--- utils/src/main/java/org/craftercms/bundle/utils/actions/DownloadMongoDB.java
+++ utils/src/main/java/org/craftercms/bundle/utils/actions/DownloadMongoDB.java
@@ -12,55 +12,54 @@ import java.nio.file.Paths;
  * Created by cortiz on 4/27/17.
  */
 public class DownloadMongoDB implements Action {
+
+    public static final int BUFFER_SIZE = 1024;
+
     @Override
     public void execute(String[] args) {
         OsCheck.OSType os = OsCheck.getOperatingSystemType();
-        String builder="http://downloads.mongodb.org/@OS/mongodb-@OS-x86_64-@VERSION.@EXT";
-        switch (os){
+        String builder = "http://downloads.mongodb.org/@OS/mongodb-@OS-x86_64-@VERSION.@EXT";
+        switch (os) {
             case Windows:
-                builder=builder.replaceAll("@OS","win32")
-                        .replaceAll("@VERSION","2008plus-ssl-v3.4-latest-signed")
-                        .replaceAll("@EXT","msi");
+                builder = builder.replaceAll("@OS", "win32").replaceAll("@VERSION",
+                    "2008plus-ssl-v3.4-latest-signed").replaceAll("@EXT", "msi");
                 break;
             case MacOS:
-                builder=builder.replaceAll("@OS","osx")
-                        .replaceAll("@VERSION","3.4.4")
-                        .replaceAll("@EXT","tgz");
+                builder = builder.replaceAll("@OS", "osx").replaceAll("@VERSION", "3.4.4").replaceAll("@EXT", "tgz");
                 break;
             case Linux:
-                builder=builder.replaceAll("@OS","linux")
-                        .replaceAll("@VERSION","3.4.4")
-                        .replaceAll("@EXT","tgz");
+                builder = builder.replaceAll("@OS", "linux").replaceAll("@VERSION", "3.4.4").replaceAll("@EXT", "tgz");
                 break;
             default:
-                System.out.println("Current OS not supported, please check documentation for installing manually mongodb");
+                System.out.println("Current OS not supported, please check documentation for installing manually "
+                    + "mongodb");
         }
 
         try {
-            URL downloadUrl=new URL(builder.toString());
-            try{
+            URL downloadUrl = new URL(builder.toString());
+            try {
                 URLConnection connection = downloadUrl.openConnection();
                 InputStream input = connection.getInputStream();
-                File output=null;
-                if(OsCheck.getOperatingSystemType()== OsCheck.OSType.Windows) {
-                   output = Paths.get(".", "mongodb.msi").toFile();
-                }else{
-                   output = Paths.get(".", "mongodb.tgz").toFile();
+                File output = null;
+                if (OsCheck.getOperatingSystemType() == OsCheck.OSType.Windows) {
+                    output = Paths.get(".", "mongodb.msi").toFile();
+                } else {
+                    output = Paths.get(".", "mongodb.tgz").toFile();
                 }
                 OutputStream out = new FileOutputStream(output);
-                byte[] buffer= new byte[1024];
+                byte[] buffer = new byte[BUFFER_SIZE];
                 int n;
-                long total=0;
-                while ((n = input.read(buffer)) != -1){
-                    out.write(buffer,0,n);
-                    total+=n;
-                    System.out.printf("Downloading ... %.1f mb\r",(double)((total/1024)/1024));
-                 
+                long total = 0;
+                while ((n = input.read(buffer)) != -1) {
+                    out.write(buffer, 0, n);
+                    total += n;
+                    System.out.printf("Downloading ... %.1f mb\n", (double)((total / BUFFER_SIZE) / BUFFER_SIZE));
+
                 }
                 out.flush();
                 out.close();
                 input.close();
-            }catch (IOException ex){
+            } catch (IOException ex) {
                 ex.printStackTrace();
             }
         } catch (MalformedURLException e) {
