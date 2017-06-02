89deb57aaf9de7d322f0bcb67b0f2d6841e37aa5
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-06-02 09:04:19 -0600

    Makes sure that the wrapper script are also path agnostic

--- resources/crafter/shutdown.sh
+++ resources/crafter/shutdown.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-./crafter.sh stop
+CRAFTER_STOP_HOME=${CRAFTER_STOP_HOME:=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )}
+
+if [[ -s  "$CRAFTER_STOP_HOME/crafter.sh" ]]; then
+      $CRAFTER_STOP_HOME/crafter.sh stop
+      exit 0
+else
+      echo -e "\033[38;5;196m"
+      echo "crafter.sh was not found in $CRAFTER_STOP_HOME"
+      echo -e "\033[0m"
+      exit -1
+fi
