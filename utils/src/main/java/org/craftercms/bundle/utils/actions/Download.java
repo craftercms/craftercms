35201939013f9442e060e33ce8653237ea41e708
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-09-01 11:40:21 -0600

    Windows, Mongodb will check if is installed by msi or it will download the windows version of it.
    Signed-off-by: Carlos Ortiz <carlos.ortiz@craftercms.org>

--- utils/src/main/java/org/craftercms/bundle/utils/actions/Download.java
+++ utils/src/main/java/org/craftercms/bundle/utils/actions/Download.java
@@ -1,6 +1,11 @@
 package org.craftercms.bundle.utils.actions;
 
-import java.io.PrintStream;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.craftercms.bundle.utils.Action;
 
@@ -8,6 +13,7 @@ import org.craftercms.bundle.utils.Action;
  * Created by cortiz on 4/27/17.
  */
 public class Download implements Action{
+    private static final int BUFFER_SIZE = 1024;
     /**
      * Output.
      */
@@ -18,10 +24,45 @@ public class Download implements Action{
         if (args.length <=0 ) {
             help();
         } else{
-            if (args[0].equalsIgnoreCase("mongodb")){
-                new DownloadMongoDB().execute(args);
+            switch (args[0].toLowerCase()){
+                case "mongodb":
+                    new DownloadMongoDB().execute(args);
+                    break;
+                case "mongodbmsi":
+                    new DownloadMongoMSIDB().execute(args);
+                    break;
+                default:
+                    URL downloadUrl = null;
+                    try {
+                        downloadUrl = new URL((args[0]));
+                        downloadUrl(downloadUrl, Paths.get(".", downloadUrl.getFile()));
+                    } catch (MalformedURLException e) {
+                        System.out.println(args[0]+ " is not a valid url");
+                    }
             }
         }
+    }
+
+
+    protected void downloadUrl(final URL downloadUrl, final Path saveTo){
+            try {
+                URLConnection connection = downloadUrl.openConnection();
+                InputStream input = connection.getInputStream();
+                OutputStream out = new FileOutputStream(saveTo.toFile());
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int n;
+                long total = 0;
+                System.out.println("Downloading "+downloadUrl.toString()+" please wait.");
+                while ((n = input.read(buffer)) != -1) {
+                    out.write(buffer, 0, n);
+                    total += n;
+                }
+                out.flush();
+                out.close();
+                input.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
     }
     @Override
     public void help() {
