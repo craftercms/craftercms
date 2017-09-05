67075eedac2ecc7e816fdb0b481f009fb6999dc4
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-09-05 09:45:36 -0600

    Solves #1266

--- resources/crafter/crafter.bat
+++ resources/crafter/crafter.bat
@@ -1,45 +1,12 @@
 @echo off
-
 rem Make sure this variable is clean.
 SET CRAFTER_BIN_FOLDER=
 SET CATALINA_OPTS=
 rem Reinit variables
 SET CRAFTER_BIN_FOLDER=%~dp0
 for %%i in ("%~dp0..") do set CRAFTER_HOME=%%~fi\
-SET DEPLOYER_HOME=%CRAFTER_BIN_FOLDER%\crafter-deployer
 
-SET CATALINA_HOME=%CRAFTER_BIN_FOLDER%apache-tomcat
-SET CATALINA_PID=%CATALINA_HOME%\tomcat.pid
-SET CATALINA_LOGS_DIR=%CRAFTER_HOME%logs\tomcat
-SET CATALINA_OUT=%CATALINA_LOGS_DIR%catalina.out
-SET CRAFTER_APPLICATION_LOGS=%CATALINA_LOGS_DIR%
-SET CATALINA_OPTS=-Dcatalina.logs=%CATALINA_LOGS_DIR% -server -Xss1024K -Xms1G -Xmx4G -Dapplication.logs=%CRAFTER_APPLICATION_LOGS%
-SET SOLR_PORT=@SOLR_PORT@
-SET SOLR_DEBUG_PORT=@SOLR_PORT_D@
-SET SOLR_INDEXES_DIR=%CRAFTER_HOME%data\indexes
-SET SOLR_LOGS_DIR=%CRAFTER_HOME%logs\solr
-SET SOLR_OPTS=-server -Xss1024K -Xmx1G
-SET DEPLOYER_PORT=@DEPLOYER_PORT@
-SET DEPLOYER_DEBUG_PORT=@DEPLOYER_D_PORT@
-SET DEPLOYER_DATA_DIR=%CRAFTER_HOME%data\deployer
-SET DEPLOYER_TARGET_DIR=%DEPLOYER_DATA_DIR%\targets
-SET DEPLOYER_PRODCESSED_COMMITS_DIR=%DEPLOYER_DATA_DIR%\processed-commits
-SET DEPLOYER_LOGS_DIR=%CRAFTER_HOME%logs\deployer
-SET DEPLOYER_DEPLOYMENTS_DIR=%CRAFTER_HOME%\data\repos\sites
-SET DEPLOYER_SDOUT=%DEPLOYER_LOGS_DIR%\crafter-deployer.out
-SET DEPLOYER_JAVA_OPTS=-server -Xss1024K -Xmx1G
-SET MONGODB_PORT=@MONGODB_PORT@
-SET MONGODB_HOME=%CRAFTER_HOME%mongodb
-SET MONGODB_PID=%CRAFTER_HOME%data\mongodb\mongod.lock
-SET MONGODB_DATA_DIR=%CRAFTER_HOME%data\mongodb
-SET MONGODB_LOGS_DIR=%CRAFTER_HOME%logs\mongodb
-SET TOMCAT_HTTP_PORT=@TOMCAT_HTTP_PORT@
-SET MYSQL_DATA=%CRAFTER_HOME%data\db
-SET MYSQL_PID_FILE_NAME=
-SET DEPLOYER_WIN_TITLE="Crafter Deployer @ENV@"
-SET DEPLOYER_STARTUP=startup.bat
-SET DEPLOYER_SHUTDOWN=shutdown.bat
-SET DEPLOYER_DEBUG=debug.bat
+call %CRAFTER_BIN_FOLDER%\crafter-setenv.bat
 
 IF /i "%1%"=="start" goto init
 IF /i "%1%"=="-s" goto init
@@ -90,7 +57,7 @@ start %mongoDir%\bin\mongod --dbpath=%MONGODB_DATA_DIR% --directoryperdb --journ
 IF NOT EXIST "%CRAFTER_HOME%\data\indexes" mkdir %CRAFTER_HOME%\data\indexes
 start %CRAFTER_BIN_FOLDER%solr\bin\solr start -f -p %SOLR_PORT% -Dcrafter.solr.index=%CRAFTER_HOME%\data\indexes
 call %CATALINA_HOME%\bin\startup.bat
-goto cleanOnExit
+goto cleanOnExitKeepTermAlive
 
 :debug
 set mongoDir=%CRAFTER_BIN_FOLDER%mongodb
@@ -258,13 +225,18 @@ goto cleanOnExit
 
 
 :skill
-start %CRAFTER_BIN_FOLDER%solr\bin\solr stop -f -p %SOLR_PORT%
+call %CRAFTER_BIN_FOLDER%solr\bin\solr stop -f -p %SOLR_PORT%
 taskkill /IM mongod.exe
 call %CATALINA_HOME%\bin\shutdown.bat
-start %DEPLOYER_HOME%\%DEPLOYER_SHUTDOWN%
+call %DEPLOYER_HOME%\%DEPLOYER_SHUTDOWN%
+taskkill /FI "WINDOWTITLE eq \"Solr-%SOLR_PORT%\"
 goto cleanOnExit
 
 
 :cleanOnExit
 cd %CRAFTER_BIN_FOLDER%
-exit /b 0
+exit
+
+:cleanOnExitKeepTermAlive
+cd %CRAFTER_BIN_FOLDER%
+exit
