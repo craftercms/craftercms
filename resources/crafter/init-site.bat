13ca43387f200f9b3cd6a22055e370b58299f91b
Carlos Ortiz <carlos.ortiz@craftercms.org>
2017-09-22 13:49:35 -0600

    Solves 1330

--- resources/crafter/init-site.bat
+++ resources/crafter/init-site.bat
@@ -17,10 +17,10 @@ set REPO=%2
 IF NOT DEFINED REPO SET REPO=%AUTHORING_SITE_REPOS%\%SITE%\published
 
 echo "Creating Solr Core"
-java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:@TOMCAT_HTTP_PORT@/crafter-search/api/2/admin/index/create" "{""id"":""%SITE%""}"
+java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:@TOMCAT_HTTP_PORT@/crafter-search/api/2/admin/index/create" "{""id"":""%SITE%""}" 2> nul
 echo ""
 echo "Creating Deployer Target"
-java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:@DEPLOYER_PORT@/api/1/target/create"  "{""env"":""default"", ""site_name"":""%SITE%"", ""template_name"":""remote"", ""repo_url"":""%REPO%"", ""repo_branch"":""live"", ""engine_url"":""http://localhost:@TOMCAT_HTTP_PORT@""}"
+java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:@DEPLOYER_PORT@/api/1/target/create"  "{""env"":""default"", ""site_name"":""%SITE%"", ""template_name"":""remote"", ""repo_url"":""%REPO%"", ""repo_branch"":""live"", ""engine_url"":""http://localhost:@TOMCAT_HTTP_PORT@""}" 2> nul
 exit /b 0
 :shelp
 echo "Usage: init-site.sh <site name> [site's published repo git url]"
