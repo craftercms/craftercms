1d225d8b59e64174c9fcdad5f81842567874d2a1
Jose Ross <jdross17@gmail.com>
2017-11-07 13:32:37 -0600

    Fixed port placeholders

--- resources/crafter/init-site.bat
+++ resources/crafter/init-site.bat
@@ -20,9 +20,9 @@ IF NOT DEFINED REPO SET REPO=%AUTHORING_SITE_REPOS%\%SITE%\published
 IF DEFINED PRIVATE_KEY SET PRIVATE_KEY=, ""ssh_private_key_path"":""%3%""
 
 echo "Creating Solr Core"
-java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:9080/crafter-search/api/2/admin/index/create" "{""id"":""%SITE%""}" > nul
+java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:@TOMCAT_HTTP_PORT@/crafter-search/api/2/admin/index/create" "{""id"":""%SITE%""}" > nul
 echo "Creating Deployer Target"
-java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:9192/api/1/target/create"  "{""env"":""default"", ""site_name"":""%SITE%"", ""template_name"":""remote"", ""repo_url"":""%REPO%"", ""repo_branch"":""live"", ""engine_url"":""http://localhost:9080"" %PRIVATE_KEY% }" > nul
+java -jar %DELIVERY_HOME%craftercms-utils.jar post "http://localhost:@DEPLOYER_PORT@/api/1/target/create"  "{""env"":""default"", ""site_name"":""%SITE%"", ""template_name"":""remote"", ""repo_url"":""%REPO%"", ""repo_branch"":""live"", ""engine_url"":""http://localhost:@TOMCAT_HTTP_PORT@"" %PRIVATE_KEY% }" > nul
 exit /b 0
 :shelp
 echo "Usage: init-site.sh <site name> [site's published repo git url] [ssh private key path]"
