5e07fdd33f029dd9a71d72499d299a9873bc5c4b
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-05-31 16:46:50 -0400

    Fixed script calling the wrong groovy file.

--- resources/crafter/upgrade.bat
+++ resources/crafter/upgrade.bat
@@ -7,4 +7,4 @@ SET CRAFTER_ROOT=%CRAFTER_HOME%\..
 call %CRAFTER_HOME%\crafter-setenv.bat
 
 REM Execute Groovy script
-%CRAFTER_HOME%\groovy\bin\groovy -Dgrape.root=%CRAFTER_HOME% %CRAFTER_HOME%\init-site.groovy %*
+%CRAFTER_HOME%\groovy\bin\groovy -Dgrape.root=%CRAFTER_HOME% %CRAFTER_HOME%\upgrade.groovy %*
