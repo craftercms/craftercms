43ca99295e2fec36918cf0acd8b41528618d2e1a
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2018-06-27 13:26:25 -0400

    Fixed upgrade bat script.

--- resources/crafter/upgrade/upgrade.bat
+++ resources/crafter/upgrade/upgrade.bat
@@ -9,6 +9,6 @@ SET UPGRADE_TMP_DIR=%CRAFTER_ROOT%\upgrade
 call %CRAFTER_HOME%\crafter-setenv.bat
 
 REM Execute Groovy script
-%CRAFTER_HOME%\groovy\bin\groovy -cp %CRAFTER_HOME% -Dgrape.root=%CRAFTER_HOME% %UPGRADE_HOME%\setup.groovy
-%UPGRADE_TMP_DIR%\groovy\bin\groovy -cp %UPGRADE_TMP_DIR% -Dgrape.root=%UPGRADE_TMP_DIR% %UPGRADE_TMP_DIR%\upgrade.groovy %*
-%CRAFTER_HOME%\groovy\bin\groovy -cp %CRAFTER_HOME% -Dgrape.root=%CRAFTER_HOME% %UPGRADE_HOME%\cleanup.groovy
+call %CRAFTER_HOME%\groovy\bin\groovy -cp %CRAFTER_HOME% -Dgrape.root=%CRAFTER_HOME% %UPGRADE_HOME%\setup.groovy
+call %UPGRADE_TMP_DIR%\groovy\bin\groovy -cp %UPGRADE_TMP_DIR% -Dgrape.root=%UPGRADE_TMP_DIR% %UPGRADE_TMP_DIR%\upgrade.groovy %*
+call %CRAFTER_HOME%\groovy\bin\groovy -cp %CRAFTER_HOME% -Dgrape.root=%CRAFTER_HOME% %UPGRADE_HOME%\cleanup.groovy
