b7525e665ebe8f32c05286239f97abe862ebf981
Jose Ross <jdross17@gmail.com>
2020-06-03 08:57:43 -0600

    Fix upgrade from 3.0.x

--- resources/bin/upgrade/hooks/v30/RecreateSolrCoresHook.groovy
+++ resources/bin/upgrade/hooks/v30/RecreateSolrCoresHook.groovy
@@ -60,8 +60,10 @@ class RecreateSolrCoresHook implements PostUpgradeHook {
         def newCoreName = environment == "authoring" ? "${siteName}-preview" : siteName
         def deployerEnv = environment == "authoring" ? 'preview' : 'default'
 
+        // delete existing core from 3.0.x
         deleteCore(oldCoreName)
-        createCore(newCoreName)
+
+        // reindex using the new core created by studio
         reindexContent(siteName, deployerEnv)
     }
 
@@ -81,27 +83,6 @@ class RecreateSolrCoresHook implements PostUpgradeHook {
             }
         }
     }
-
-    private void createCore(String coreName) {
-        def httpClient = configure {
-            request.uri = getTomcatUrl()
-        }
-
-        httpClient.post {
-            request.uri.path = "/crafter-search/api/2/admin/index/create"
-            request.contentType = 'application/json'
-            request.body = [
-                    id: coreName
-            ]
-            response.success { fs ->
-                println "Solr core '${coreName}' created successfully"
-            }
-            response.failure { fs, body ->
-                throw new UpgradeException("Error while creating Solr core '${coreName}': ${body.message}")
-            }
-        }
-    }
-
 
     private void reindexContent(String siteName, String deployerEnv) {
         def httpClient = configure {
