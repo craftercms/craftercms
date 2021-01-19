d8e53eae34bf258caf08acd7373be76db2cb647e
Sumer Jabri <sumer.jabri@craftercms.org>
2021-01-19 15:32:18 -0500

    Updated Travis CI.

--- .travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	bash -c 'mvn -DskipTests --settings .travis-settings.xml deploy';
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	bash -c 'mvn -DskipTests clean install';
-fi
