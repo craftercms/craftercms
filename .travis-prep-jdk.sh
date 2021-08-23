45b4aa06673d2e8dbcced89c2d584c8065629ab0
Sumer Jabri <sumer.jabri@craftercms.org>
2021-08-23 13:51:27 -0400

    Updated CI settings.

--- .travis-prep-jdk.sh
+++ .travis-prep-jdk.sh
@@ -7,6 +7,7 @@ if [ -f "$HOME/downloads/OpenJDK11U-jdk_x64_linux_11.0.12_7.tar.gz" ]
 then
 	echo JDK already downloaded
 else
+	echo Downloading JDK from $JDK_URL
 	pushd .
 	mkdir -p "$HOME/downloads"
 	cd "$HOME/downloads"
@@ -18,14 +19,20 @@ if [ -f "$HOME/jdk/bin/java" ]
 then
 	echo Java already installed
 else
+	echo Unzipping the JDK
 	pushd .
 	cd "$HOME/downloads"
-	tar xvzf "$JDK_FILE"
+	tar xzf "$JDK_FILE"
 	if [ -f "$HOME/jdk" ]
 	then
+		echo JDK folder found, moving the into JDK home
 		mv "$HOME/downloads/$JDK_FOLDER/*" "$HOME/jdk/"
 	else
+		echo JDK home not found, creating and moving JDK into home
 		mv "$HOME/downloads/$JDK_FOLDER" "$HOME/jdk"
 	fi
 	popd
 fi
+
+ls -lh $HOME/downloads
+ls -lh $HOME/jdk
