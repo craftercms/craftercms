ac102e879818bfbf5233c797fffb301476befa40
Sumer Jabri <sumer.jabri@craftercms.org>
2021-08-25 10:54:29 -0400

    Fixed https://github.com/craftercms/craftercms/issues/4965

--- resources/env/authoring/tomcat-config/setenv.sh
+++ resources/env/authoring/tomcat-config/setenv.sh
@@ -9,7 +9,8 @@ fi
 
 # Opts used only in Tomcat start
 CATALINA_OPTS="$CATALINA_OPTS -Dtomcat.host=$TOMCAT_HOST -Dtomcat.http.port=$TOMCAT_HTTP_PORT \
-  -Dtomcat.https.port=$TOMCAT_HTTPS_PORT -Dtomcat.ajp.port=$TOMCAT_AJP_PORT -Dcrafter.home=$CRAFTER_HOME \
+  -Dtomcat.https.port=$TOMCAT_HTTPS_PORT -Dtomcat.ajp.port=$TOMCAT_AJP_PORT \
+  -Dtomcat.debug.port=$TOMCAT_DEBUG_PORT -Dcrafter.home=$CRAFTER_HOME \
   -Dcrafter.bin.dir=$CRAFTER_BIN_DIR -Dcrafter.data.dir=$CRAFTER_DATA_DIR -Dcrafter.logs.dir=$CRAFTER_LOGS_DIR \
   -Dcatalina.logs=$CATALINA_LOGS_DIR -Dapplication.logs=$CATALINA_LOGS_DIR -Djava.net.preferIPv4Stack=true \
 	-Dgrape.root=$CRAFTER_BIN_DIR"
