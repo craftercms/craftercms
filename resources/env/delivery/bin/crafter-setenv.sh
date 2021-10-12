8b53ec55d113b1e5d263fbcb492263235c9fce1d
Sumer Jabri <sumer.jabri@craftercms.org>
2021-10-12 12:35:13 -0400

    Adjusted Elasticsearch JVM options to flow from crafter-setenv.sh.

--- resources/env/delivery/bin/crafter-setenv.sh
+++ resources/env/delivery/bin/crafter-setenv.sh
@@ -44,7 +44,7 @@ export PROFILE_URL=${PROFILE_URL:="http://$TOMCAT_HOST:$TOMCAT_HTTP_PORT/crafter
 export SOCIAL_URL=${SOCIAL_URL:="http://$TOMCAT_HOST:$TOMCAT_HTTP_PORT/crafter-social"}
 
 # -------------------- Java opts --------------------
-export ES_JAVA_OPTS=${ES_JAVA_OPTS:="-server -Xss1024K -Xmx1G"}
+export ES_JAVA_OPTS=${ES_JAVA_OPTS:="-server -Xss1024K -Xms1G -Xmx1G"}
 export DEPLOYER_JAVA_OPTS=${DEPLOYER_JAVA_OPTS:="-server -Xss1024K -Xmx1G"}
 export CATALINA_OPTS=${CATALINA_OPTS:="-server -Xss1024K -Xms1G -Xmx2G"}
 
