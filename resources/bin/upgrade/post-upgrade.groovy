7a23a96425ed290e95b0c6110cc67192fba59338
Sumer Jabri <sumer.jabri@craftercms.org>
2022-09-28 16:22:27 -0400

    Upgraded Apache Commons Configuration 2.

--- resources/bin/upgrade/post-upgrade.groovy
+++ resources/bin/upgrade/post-upgrade.groovy
@@ -19,7 +19,7 @@ package upgrade
         @Grab(group = 'org.slf4j', module = 'slf4j-nop', version = '1.7.25'),
         @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.7'),
         @Grab(group = 'org.apache.commons', module = 'commons-collections4', version = '4.1'),
-        @Grab(group = 'org.apache.commons', module = 'commons-configuration2', version = '2.7'),
+        @Grab(group = 'org.apache.commons', module = 'commons-configuration2', version = '2.8.0'),
         @Grab(group = 'commons-beanutils', module = 'commons-beanutils', version = '1.9.3'),
         @Grab(group = 'org.yaml', module = 'snakeyaml', version = '1.24'),
         @Grab(group = 'io.github.http-builder-ng', module = 'http-builder-ng-core', version = '1.0.3')
