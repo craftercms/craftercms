608e19d02598764451ea9c85bee88b2036691627
Sumer Jabri <sumer.jabri@craftercms.org>
2024-05-09 10:26:55 -0400

    Optimized the bundle task to not run if not needed.

--- build.gradle
+++ build.gradle
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
