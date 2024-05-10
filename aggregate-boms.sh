70aae12cce2f289a4fe36d812f26d8f07642f7e1
rart <royart@me.com>
2024-05-10 19:26:22 +0200

    [742e] SBOM Generation craftersoftware/craftercms#742

--- aggregate-boms.sh
+++ aggregate-boms.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 mkdir -p target
-cyclonedx-cli merge --output-file target/bom.json --input-format json --output-format json --input-files $(find src -name 'bom.json')
+
+if command -v cyclonedx-cli &> /dev/null; then
+  cyclonedx-cli merge --output-file target/bom.json --input-format json --output-format json --input-files $(find src -name 'bom.json')
+elif command -v cyclonedx &> /dev/null; then
+  cyclonedx merge --output-file target/bom.json --input-format json --output-format json --input-files $(find src -name 'bom.json')
+else
+  echo "CycloneDX CLI is not installed."
+fi
