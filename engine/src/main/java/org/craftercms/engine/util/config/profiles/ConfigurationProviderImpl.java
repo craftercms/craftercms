9342d19d0c72fc9613f5206b5db17464121dc127
Jonathan Mendez <jonnathan.mendez@gmail.com>
2026-09-07 13:33:11 -0600

    SiteItemService return SiteItem only when matching allowed regexes Adding exists() method for configuration support

--- engine/src/main/java/org/craftercms/engine/util/config/profiles/ConfigurationProviderImpl.java
+++ engine/src/main/java/org/craftercms/engine/util/config/profiles/ConfigurationProviderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2024 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2026 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -41,7 +41,7 @@ public class ConfigurationProviderImpl implements ConfigurationProvider {
 
     @Override
     public boolean configExists(String path) {
-        return siteItemService.getSiteItem(path) != null;
+        return siteItemService.exists(path);
     }
 
     @Override
