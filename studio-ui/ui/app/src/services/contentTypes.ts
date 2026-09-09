7f64609d61fdc457672b9cd634f67591b8d94505
jvega190 <josevega19@gmail.com>
2026-09-09 14:07:40 -0600

    Enforce previewImage path to have directory separator

--- studio-ui/ui/app/src/services/contentTypes.ts
+++ studio-ui/ui/app/src/services/contentTypes.ts
@@ -30,7 +30,7 @@ import {
 	ValidationKeys
 } from '../models/ContentType';
 import { LookupTable } from '../models/LookupTable';
-import { camelize, capitalize, isBlank, toColor } from '../utils/string';
+import { camelize, capitalize, ensureSingleSlash, isBlank, toColor } from '../utils/string';
 import { Observable, of } from 'rxjs';
 import { CONTENT_TYPE_JSON, get, getBinary, getGlobalHeaders, post } from '../utils/ajax';
 import { map, switchMap } from 'rxjs/operators';
@@ -679,7 +679,7 @@ export function fetchContentTypePreviewImageUrl(
 	thumbnailFileName?: string
 ): Observable<string> {
 	if (thumbnailFileName) {
-		const path = `/config/studio/content-types${contentTypeId}/${thumbnailFileName}`;
+		const path = ensureSingleSlash(`/config/studio/content-types/${contentTypeId}/${thumbnailFileName}`);
 		return getBinary(
 			`/studio/api/2/content/get_content_by_commit_id${toQueryString({ siteId: site, path, commitId: 'HEAD' })}`,
 			void 0,
